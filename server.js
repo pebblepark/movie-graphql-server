535d224dcc986cbbb0f4216b30afd43675a18e2f
pebblepark <subling02@gmail.com>
2022-07-18 23:35:39 +0900

    Feat: Resolvers 추가

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 import { ApolloServer, gql } from 'apollo-server';
 
+const tweets = [
+  {
+    id: '1',
+    text: 'first one',
+  },
+  {
+    id: '2',
+    text: 'second one',
+  },
+];
+
 const typeDefs = gql`
   type User {
     id: ID!
@@ -10,11 +21,12 @@ const typeDefs = gql`
   type Tweet {
     id: ID!
     text: String!
-    author: User!
+    author: User
   }
   type Query {
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
+    ping: String!
   }
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet!
@@ -22,7 +34,18 @@ const typeDefs = gql`
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
