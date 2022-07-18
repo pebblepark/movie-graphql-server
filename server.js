ac06149ae738910463626355a449dfe53aae0c26
pebblepark <subling02@gmail.com>
2022-07-18 22:33:51 +0900

    Feat: MutationType 정의

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const typeDefs = gql`
     allTweets: [Tweet]
     tweet(id: ID): Tweet
   }
+  type Mutation {
+    postTweet(text: String, userId: ID): Tweet
+    deleteTweet(id: ID): Boolean
+  }
 `;
 
 const server = new ApolloServer({ typeDefs });
